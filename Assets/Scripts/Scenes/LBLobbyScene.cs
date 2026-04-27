4fb1700893cac84d9035cc8549358fb4474d8d5f
chvj7567 <chvj7567@naver.com>
2026-04-27 12:16:22 +0900

    refactor: LBLobbyScene 생성, First.cs 제거, 배경 루프 제거

---
+++ Assets/Scripts/Scenes/LBLobbyScene.cs
@@ -0,0 +1,246 @@
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LBLobbyScene : MonoBehaviour
+{
+    [SerializeField] Canvas canvas;
+    [SerializeField] GameObject stageSelect1;
+    [SerializeField] GameObject stageSelect2;
+    [SerializeField] PageMove pageMove;
+    [SerializeField] Button missionBtn;
+    [SerializeField] Button startBtn;
+    [SerializeField] Button connectGPGSBtn;
+    [SerializeField] Button logoutBtn;
+    [SerializeField] Button shopBtn;
+    [SerializeField] Button bombBtn;
+    [SerializeField] Button menuBtn;
+    [SerializeField] Button rankingBtn;
+    [SerializeField] CHAdvertise adScript;
+    [SerializeField] ReactiveProperty<bool> dataDownload = new ReactiveProperty<bool>();
+    [SerializeField] ReactiveProperty<bool> bundleDownload = new ReactiveProperty<bool>();
+    [SerializeField] GameObject guideBackground;
+    [SerializeField] Button guideBackgroundBtn;
+    [SerializeField] List<RectTransform> guideHoleList = new List<RectTransform>();
+    [SerializeField] CHTMPro guideDesc;
+    [SerializeField] CHTMPro userID;
+    [SerializeField] GameObject objWait;
+
+    CancellationTokenSource tokenSource;
+    bool initButton = false;
+    bool firstStartBtnClick = false;
+
+    LBLoginHandler _loginHandler;
+    LBTutorial _tutorial;
+
+    void InitButton()
+    {
+        if (initButton) return;
+        initButton = true;
+
+        startBtn.OnClickAsObservable().Subscribe(async _ =>
+        {
+            if (!bundleDownload.Value || !dataDownload.Value) return;
+            firstStartBtnClick = true;
+            var arg = new UIStageSelectArg();
+            arg.stageSelect += async (select) => await StageSelect(select);
+            CHMMain.UI.ShowUI(Defines.EUI.UIStageSelect, arg);
+        });
+
+        missionBtn.OnClickAsObservable().Subscribe(_ => CHMMain.UI.ShowUI(Defines.EUI.UIMission, new CHUIArg()));
+
+        logoutBtn.OnClickAsObservable().Subscribe(async _ =>
+        {
+            if (!_loginHandler.GetGPGSLogin()) return;
+            await _loginHandler.SetGPGSLogin(false, "");
+#if UNITY_ANDROID
+            CHMGPGS.Instance.Logout();
+#endif
+        });
+
+        shopBtn.OnClickAsObservable().Subscribe(_ => CHMMain.UI.ShowUI(Defines.EUI.UIShop, new CHUIArg()));
+        bombBtn.OnClickAsObservable().Subscribe(_ => CHMMain.UI.ShowUI(Defines.EUI.UISetting, new CHUIArg()));
+
+        rankingBtn.OnClickAsObservable().Subscribe(_ =>
+        {
+            if (!CHMData.Instance.GetLoginData(CHMMain.String.CatPang).connectGPGS)
+                CHMMain.UI.ShowUI(Defines.EUI.UIAlarm, new UIAlarmArg { stringID = 107 });
+            else
+                CHMMain.UI.ShowUI(Defines.EUI.UIRank, new CHUIArg());
+        });
+
+        connectGPGSBtn.OnPointerClickAsObservable().Subscribe(_ =>
+        {
+#if UNITY_ANDROID
+            if (!CHMData.Instance.GetLoginData(CHMMain.String.CatPang).connectGPGS)
+            {
+                objWait.SetActive(true);
+                CHMGPGS.Instance.Login(async (success, localUser) =>
+                {
+                    await _loginHandler.SetGPGSLogin(success, localUser.userName);
+                    objWait.SetActive(false);
+                });
+            }
+#endif
+        });
+
+        menuBtn.OnClickAsObservable().Subscribe(_ =>
+        {
+            var arg = new UIStageSelectArg();
+            arg.stageSelect += async (select) => await StageSelect(select);
+            CHMMain.UI.ShowUI(Defines.EUI.UIStageSelect, arg);
+        });
+    }
+
+    async void Start()
+    {
+        tokenSource = new CancellationTokenSource();
+
+        _loginHandler = new LBLoginHandler();
+        _loginHandler.Init(userID, connectGPGSBtn, logoutBtn, objWait, async (success) =>
+        {
+            dataDownload.Value = true;
+            if (success && firstStartBtnClick && CHMData.Instance.newUser == false)
+                await StageSelect(PlayerPrefs.GetInt(CHMMain.String.SelectStage));
+        });
+
+        _tutorial = new LBTutorial();
+        _tutorial.Init(guideBackground, guideBackgroundBtn, guideHoleList, guideDesc);
+
+        menuBtn.gameObject.SetActive(false);
+        stageSelect1.SetActive(false);
+        stageSelect2.SetActive(false);
+        startBtn.gameObject.SetActive(false);
+        missionBtn.gameObject.SetActive(false);
+        connectGPGSBtn.gameObject.SetActive(false);
+        logoutBtn.gameObject.SetActive(false);
+        shopBtn.gameObject.SetActive(false);
+        bombBtn.gameObject.SetActive(false);
+        rankingBtn.gameObject.SetActive(false);
+        objWait.SetActive(false);
+        userID.gameObject.SetActive(false);
+        foreach (var h in guideHoleList) h.gameObject.SetActive(false);
+        guideBackground.SetActive(false);
+        guideBackgroundBtn.gameObject.SetActive(false);
+
+        CHMIAP.Instance.Init();
+        CHMAdmob.Instance.Init();
+
+        dataDownload.Subscribe(async dl =>
+        {
+            if (CHMAssetBundle.Instance.firstDownload && dl && bundleDownload.Value)
+            {
+                CHMAssetBundle.Instance.firstDownload = false;
+                startBtn.gameObject.SetActive(true);
+            }
+        });
+
+        bundleDownload.Subscribe(async bl =>
+        {
+            if (bl && !dataDownload.Value)
+            {
+                if (_loginHandler.GetPhoneLoginState())
+                {
+                    objWait.SetActive(true);
+#if UNITY_ANDROID
+                    CHMGPGS.Instance.Login(async (success, localUser) =>
+                    {
+                        await _loginHandler.SetGPGSLogin(success, localUser.userName);
+                        objWait.SetActive(false);
+                    });
+#endif
+                }
+                else await _loginHandler.SetGPGSLogin(false, "");
+            }
+            else if (CHMAssetBundle.Instance.firstDownload && bl && dataDownload.Value)
+            {
+                CHMAssetBundle.Instance.firstDownload = false;
+                startBtn.gameObject.SetActive(true);
+            }
+        });
+
+        if (CHMAssetBundle.Instance.firstDownload)
+        {
+            pageMove.ActiveMoveBtn(false);
+        }
+        else
+        {
+            bundleDownload.Value = true;
+            dataDownload.Value = true;
+
+            pageMove.Init((Defines.ESelectStage)PlayerPrefs.GetInt(CHMMain.String.SelectStage));
+            stageSelect1.SetActive(true);
+            stageSelect2.SetActive(true);
+            missionBtn.gameObject.SetActive(true);
+            shopBtn.gameObject.SetActive(true);
+            bombBtn.gameObject.SetActive(true);
+            menuBtn.gameObject.SetActive(true);
+            rankingBtn.gameObject.SetActive(true);
+
+            var login = _loginHandler.GetGPGSLogin();
+            connectGPGSBtn.gameObject.SetActive(!login);
+            logoutBtn.gameObject.SetActive(login);
+
+            adScript.GetAdvertise();
+
+            var loginData = CHMData.Instance.GetLoginData(CHMMain.String.CatPang);
+            if (loginData.guideIndex == 0)
+            {
+                Time.timeScale = 0;
+                guideBackground.SetActive(true);
+                guideBackground.transform.SetAsLastSibling();
+                guideBackgroundBtn.gameObject.SetActive(true);
+                guideBackgroundBtn.transform.SetAsLastSibling();
+
+                loginData.guideIndex = await _tutorial.TutorialStart();
+
+                guideBackground.SetActive(false);
+                guideBackgroundBtn.gameObject.SetActive(false);
+                Time.timeScale = 1;
+                CHMData.Instance.SaveData(CHMMain.String.CatPang);
+            }
+        }
+
+        bundleDownload.Value = true;
+        InitButton();
+        CHMData.Instance.GetShopData("1").buy = true;
+    }
+
+    private void OnApplicationQuit() => CHMData.Instance.SaveData(CHMMain.String.CatPang);
+
+    async Task StageSelect(int select)
+    {
+        PlayerPrefs.SetInt(CHMMain.String.SelectStage, select);
+        pageMove.Init((Defines.ESelectStage)select);
+        startBtn.gameObject.SetActive(false);
+        missionBtn.gameObject.SetActive(true);
+        shopBtn.gameObject.SetActive(true);
+        bombBtn.gameObject.SetActive(true);
+        stageSelect1.SetActive(true);
+        stageSelect2.SetActive(true);
+        menuBtn.gameObject.SetActive(true);
+        rankingBtn.gameObject.SetActive(true);
+
+        CHMMain.Sound.Play(Defines.ESound.Bgm);
+
+        var loginData = CHMData.Instance.GetLoginData(CHMMain.String.CatPang);
+        if (loginData.guideIndex == 0)
+        {
+            Time.timeScale = 0;
+            guideBackground.SetActive(true);
+            guideBackground.transform.SetAsLastSibling();
+            guideBackgroundBtn.gameObject.SetActive(true);
+            guideBackgroundBtn.transform.SetAsLastSibling();
+
+            loginData.guideIndex = await _tutorial.TutorialStart();
+
+            guideBackground.SetActive(false);
+            guideBackgroundBtn.gameObject.SetActive(false);
+            Time.timeScale = 1;
+            CHMData.Instance.SaveData(CHMMain.String.CatPang);
+        }
+    }
+}
